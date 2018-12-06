f1fcc0154b933453a326aa1c2e44559865a3c747
hg1997 <554618947@qq.com>
2018-12-06 11:21:23 +0800

    新建person.java

---
+++ service-api/src/main/java/edu/cqupt/domain/person.java
@@ -0,0 +1,42 @@
+/*
+  Copyright (C), 2011-2018, 微贷网.
+ */
+package edu.cqupt.domain;
+
+/**
+ * @author wd 2018/12/6.
+ */
+public class person {
+    private String name;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "person{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public person(String name, String password) {
+
+        this.name = name;
+        this.password = password;
+    }
+}
